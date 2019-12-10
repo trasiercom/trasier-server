acf1df7ad6e10def5a922ab0f2a8ffe787005eab
Marcus Fihlon <marcus@fihlon.swiss>
2019-12-10 19:59:51 +0100

    :construction_worker: Adding Travis-CI

---
+++ deploy-to-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "We're not on the master branch, skipping docker deployment."
+  exit 0
+fi
+
+docker build -t trasier/trasier-server .
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker push trasier/trasier-server
+docker logout
