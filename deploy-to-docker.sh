b05e0c8ed865c240f9dd7d3e6e548cdf1b376159
Marcus Fihlon <marcus@fihlon.swiss>
2019-12-10 22:09:19 +0100

    :whale: Docker login without email

--- deploy-to-docker.sh
+++ deploy-to-docker.sh
@@ -2,7 +2,7 @@
 
 if [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
     docker build -t trasier/trasier-server .
-    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    docker login -u $DOCKER_USER -p $DOCKER_PASS
     docker push trasier/trasier-server
     docker logout
 else
